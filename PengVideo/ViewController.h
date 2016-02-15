ba6635b8a0591891b3586ea7b7d50b04e8d7fe5f
xiaoyunfei <455233084@qq.com>
2016-02-15 15:12:16 +0800

    Initial Commit

---
+++ PengVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PengVideo
+//
+//  Created by hengfu on 16/2/15.
+//  Copyright © 2016年 hengfu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PengVideo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PengVideo
+//
+//  Created by hengfu on 16/2/15.
+//  Copyright © 2016年 hengfu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
