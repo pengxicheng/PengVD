d13a394921bf62c80cefa3246e3211662b13a288
xiaoyunfei <455233084@qq.com>
2016-02-16 13:56:01 +0800

    精简

--- PengVideo/AppDelegate.m
+++ PengVideo/AppDelegate.m
@@ -17,7 +17,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    NSLog(@"testA提交");
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
