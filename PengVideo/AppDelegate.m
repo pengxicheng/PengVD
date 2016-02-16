d26efc603b47ab6285757d6a5c256e1b24698ac2
xiaoyunfei <455233084@qq.com>
2016-02-16 09:29:52 +0800

    1

--- PengVideo/AppDelegate.m
+++ PengVideo/AppDelegate.m
@@ -17,6 +17,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
+    RootTableViewController *rootVC = [[RootTableViewController alloc] init];
+    UINavigationController *rootNC = [[UINavigationController alloc] initWithRootViewController:rootVC];
+    self.window.rootViewController = rootNC;
     return YES;
 }
 
